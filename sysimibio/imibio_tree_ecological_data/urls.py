3eaadb070b90b42114e7e746930b3f404a220296
Felipe Barros <felipe.b4rros@gmail.com>
2021-06-02 09:24:59 -0300

    remove pkg from requirements

--- sysimibio/imibio_tree_ecological_data/urls.py
+++ sysimibio/imibio_tree_ecological_data/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from sysimibio.imibio_tree_ecological_data.views import new, detail
 
 app_name = 'imibio_tree_ecological_data'
-
+# todo crear lista de spp por parcela
+# todo crear lista spp por campo
 urlpatterns = [
     path('', new, name='new'),
     path('<int:pk>/', detail, name='detail'),
