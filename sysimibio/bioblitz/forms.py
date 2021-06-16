f4354394056c64c96dc337de7e4757a7fd662b94
Felipe Barros <felipe.b4rros@gmail.com>
2021-06-16 10:15:58 -0300

    big fiz on bioblitz clean form

--- sysimibio/bioblitz/forms.py
+++ sysimibio/bioblitz/forms.py
@@ -11,5 +11,5 @@ class BioblitzModelForm(ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        if not cleaned_data.get('project_slug') and cleaned_data.get('project_id'): # todo no debería ser OR?
+        if not cleaned_data.get('project_slug') and not cleaned_data.get('project_id'):
             raise ValidationError("Informar nombre del proyecto o id")
